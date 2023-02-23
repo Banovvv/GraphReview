d30687d22a6fdfccf42c814c387749f6300319e1
Ivan Gechev <ivan_banov@abv.bg>
2023-02-23 19:32:05 +0200

    Abstractions, Models and Tests

---
+++ Core/GraphReview.Application/Models/EmailObject.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.GuardClauses;
+
+namespace GraphReview.Application.Models
+{
+    public class EmailObject
+    {
+        public EmailObject(
+            string from,
+            string replyTo,
+            string subject,
+            string body,
+            List<string> recipients)
+        {
+            From = Guard.Against.NullOrWhiteSpace(from);
+            ReplyTo = Guard.Against.NullOrWhiteSpace(replyTo);
+            Subject = Guard.Against.NullOrWhiteSpace(subject);
+            Body = Guard.Against.NullOrWhiteSpace(body);
+            Recipients = Guard.Against.Null(recipients);
+        }
+
+        public string From { get; set; }
+        public string ReplyTo { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public List<string> Recipients { get; set; }
+        public List<string>? Ccs { get; set; }
+        public List<string>? Bccs { get; set; }
+        public List<string>? Attachments { get; set; }
+        public List<string>? InlineAttachments { get; set; }
+    }
+}
