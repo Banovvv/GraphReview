7411c9e23bf187e2a426365cbf67ea6d6bdc3da9
Ivan Gechev <ivan_banov@abv.bg>
2023-02-28 10:45:45 +0200

    DepartmentTests

---
+++ Tests/GraphReview.Domain.Tests/Models/DepartmentTests.cs
@@ -0,0 +1,22 @@
+﻿using GraphReview.Domain.Models;
+
+namespace GraphReview.Domain.Tests.Models
+{
+    public class DepartmentTests
+    {
+        [Fact]
+        public void GivenCorrectParamaters_WhenConstructorIsInvoked_ADepartmentIsCreated()
+        {
+            // Arrange
+            var name = "Development";
+
+            // Act
+            var department = new Department(name);
+
+            // Assert
+            department.Should().NotBeNull();
+            department.Id.Should().NotBeNullOrWhiteSpace();
+            department.Name.Should().Be(name);
+        }
+    }
+}
