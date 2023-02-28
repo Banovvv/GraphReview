f628c7c57f87680dd4d81e392e4169bda710df7d
Ivan Gechev <ivan_banov@abv.bg>
2023-02-28 10:25:29 +0200

    Review model

---
+++ Core/GraphReview.Domain/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿namespace GraphReview.Domain.Models
+{
+    public class Review
+    {
+        public string Id { get; set; }
+
+        public string ReviewerId { get; set; }
+        public virtual Employee Reviewer { get; set; }
+
+        public string RevieweeId { get; set; }
+        public virtual Employee Reviewee { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Duration { get; set; }
+    }
+}
