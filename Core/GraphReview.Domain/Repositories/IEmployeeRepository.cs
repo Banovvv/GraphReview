95009474fd14ec1f70ad78daaac173cfd9a6c03a
Ivan Gechev <ivan_banov@abv.bg>
2023-03-01 13:11:30 +0200

    Interface fiddling

---
+++ Core/GraphReview.Domain/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,8 @@
+﻿using GraphReview.Domain.Models;
+
+namespace GraphReview.Domain.Repositories
+{
+    public interface IEmployeeRepository : IGenericRepository<Employee>
+    {
+    }
+}
--- Core/GraphReview.Domain/UnitOfWork/IUnitOfWork.cs
+++ Core/GraphReview.Domain/UnitOfWork/IUnitOfWork.cs
@@ -1,7 +1,12 @@
-﻿namespace GraphReview.Domain.UnitOfWork
+﻿using GraphReview.Domain.Repositories;
+
+namespace GraphReview.Domain.UnitOfWork
 {
     public interface IUnitOfWork : IDisposable
     {
+        IReviewRepository ReviewRepository { get; }
+        IEmployeeRepository EmployeeRepository { get; }
+        IDepartmentRepository DepartmentRepository { get; }
         Task<int> SaveChangesAsync();
     }
 }
