37d7dd2beca9d88c383f7abc55985c6b9ea83320
Ivan Gechev <ivan_banov@abv.bg>
2023-03-03 10:03:55 +0200

    ReviewServiceTests

--- Tests/GraphReview.Application.Tests/Services/ReviewServiceTests.cs
+++ Tests/GraphReview.Application.Tests/Services/ReviewServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using AutoFixture;
 using GraphReview.Application.Services;
 using GraphReview.Application.Tests.Helpers;
+using GraphReview.Domain.Exceptions;
 using GraphReview.Domain.Models;
 using GraphReview.Domain.Repositories;
 using GraphReview.Domain.UnitOfWork;
@@ -11,7 +12,7 @@ namespace GraphReview.Application.Tests.Services
     public class ReviewServiceTests
     {
         private readonly IFixture _fixture;
-        private ReviewService _reviewService;
+        private readonly ReviewService _reviewService;
         private readonly Mock<IUnitOfWork> _unitOfWork;
         private readonly Mock<IReviewRepository> _reviewRepository;
 
@@ -33,7 +34,7 @@ namespace GraphReview.Application.Tests.Services
         {
             // Arrange
             var review = _fixture.Build<Review>().Create();
-            _unitOfWork.Setup(x=>x.ReviewRepository.GetByIdAsync(review.Id, default)).ReturnsAsync(review);
+            _unitOfWork.Setup(x => x.ReviewRepository.GetByIdAsync(review.Id, default)).ReturnsAsync(review);
 
             // Act
             var result = await _reviewService.GetByIdAsync(review.Id);
@@ -41,6 +42,80 @@ namespace GraphReview.Application.Tests.Services
             // Assert
             result.Should().NotBeNull();
             result.Id.Should().Be(review.Id);
+        }
+
+        [Fact]
+        public async Task GivenInvalidId_WhenGetByIdAsyncIsInvoked_ThenExceptionIsThrown()
+        {
+            // Arrange
+            var id = Guid.NewGuid().ToString();
+            _unitOfWork.Setup(x => x.ReviewRepository.GetByIdAsync(id, default)).ReturnsAsync((Review)null);
+
+            // Act
+            Func<Task> act = async () => await _reviewService.GetByIdAsync(id);
+
+            // Assert
+            await act.Should()
+                .ThrowAsync<ReviewNotFoundException>()
+                .WithMessage("Review not found!");
+        }
+
+        [Fact]
+        public async Task WhenGetAllAsyncIsInvoked_ThenFullListOfEmployeesIsReturned()
+        {
+            // Arrange
+            var reviews = _fixture.Build<Review>().CreateMany(4);
+            _unitOfWork.Setup(x => x.ReviewRepository.GetAllAsync(default)).ReturnsAsync(reviews);
+
+            // Act
+            var result = await _reviewService.GetAllAsync();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Count().Should().Be(4);
+        }
+
+        [Fact]
+        public async Task GivenValidId_WhenDeleteAsyncIsInvoked_ThenEntityIsDeleted()
+        {
+            // Arrange
+            var review = _fixture.Build<Review>().Create();
+            _unitOfWork.Setup(x => x.ReviewRepository.GetByIdAsync(review.Id, default)).ReturnsAsync(review);
+
+            // Act
+            var result = await _reviewService.DeleteAsync(review.Id);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task GivenInvalidId_WhenDeleteAsyncIsInvoked_ThenExceptionIsThrown()
+        {
+            // Arrange
+            var id = Guid.NewGuid().ToString();
+            _unitOfWork.Setup(x => x.ReviewRepository.GetByIdAsync(id, default)).ReturnsAsync((Review)null);
+
+            // Act
+            Func<Task> act = async () => await _reviewService.DeleteAsync(id);
+
+            // Assert
+            await act.Should()
+                .ThrowAsync<ReviewNotFoundException>()
+                .WithMessage("Review not found!");
+        }
+
+        [Fact]
+        public async Task GivenValidEntity_WhenAddAsyncIsInvoked_ThenEntityIsAdded()
+        {
+            // Arrange
+            var review = _fixture.Build<Review>().Create();
+
+            // Act
+            var result = await _reviewService.AddAsync(review);
+
+            // Assert
+            result.Should().BeTrue();
         }
     }
 }
