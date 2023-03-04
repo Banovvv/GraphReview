eb6e7d30c67bac06b6f58740ebcf2e6165a1b038
Ivan Gechev <ivan_banov@abv.bg>
2023-03-04 08:53:16 +0200

    Setting up EmailService

--- Core/GraphReview.Application/Services/EmailService.cs
+++ Core/GraphReview.Application/Services/EmailService.cs
@@ -1,11 +1,36 @@
-﻿using GraphReview.Application.Abstractions.Email;
+﻿using Azure.Identity;
+using GraphReview.Application.Abstractions.Email;
 using GraphReview.Application.Models;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Graph;
 
 namespace GraphReview.Application.Services
 {
     public class EmailService : IEmailService
     {
+        private readonly IConfiguration _configuration;
+        private readonly GraphServiceClient _graphClient;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            var options = new TokenCredentialOptions()
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
+
+            var token = new ClientSecretCredential(
+                _configuration["MicrosofrGraph:TenantId"],
+                _configuration["MicrosofrGraph:ClientId"],
+                _configuration["MicrosofrGraph:ClientSecret"],
+                options);
+
+            _graphClient = new GraphServiceClient(
+                token,
+                new[] { "https://graph.microsoft.com/.default" });
+        }
+
         public Task<Event> ScheduleEventAsync()
         {
             throw new NotImplementedException();
--- External/GraphReview.Contracts/Department/DepartmentResponse.cs
+++ External/GraphReview.Contracts/Department/DepartmentResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GraphReview.Contracts.Department
+﻿namespace GraphReview.Contracts.Department
 {
     public class DepartmentResponse
     {
