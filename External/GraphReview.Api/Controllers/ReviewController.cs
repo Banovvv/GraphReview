a61b640fa0b3a5a264e3c9e240bc63d0062fe788
Ivan Gechev <ivan_banov@abv.bg>
2023-03-05 16:59:40 +0200

    ReviewController WIP

---
+++ External/GraphReview.Api/Controllers/ReviewController.cs
@@ -0,0 +1,37 @@
+﻿using GraphReview.Application.Abstractions.Email;
+using GraphReview.Application.Abstractions.Reviews;
+using GraphReview.Contracts.Review;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphReview.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+        private readonly IReviewService _reviewService;
+
+        public ReviewController(IReviewService reviewService, IEmailService emailService)
+        {
+            _emailService = emailService;
+            _reviewService = reviewService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<IEnumerable<ReviewResponse>>> GetAllAsync()
+        {
+            var reviews = await _reviewService.GetAllAsync();
+
+            var response = reviews
+                .Select(x => new ReviewResponse()
+                {
+                    Id = x.Id,
+                    StartTime = x.StartTime,
+                    EndTime = x.EndTime
+                });
+
+            return Ok(response);
+        }
+    }
+}
---
+++ External/GraphReview.Contracts/Review/ReviewResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphReview.Contracts.Review
+{
+    public class ReviewResponse
+    {
+        public string Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
