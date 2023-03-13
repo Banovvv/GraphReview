374c8e2b7b853a3f73ef836e0e2450c4671d6b89
Ivan Gechev <ivan_banov@abv.bg>
2023-03-13 16:56:54 +0200

    Cleaned some files

--- Core/GraphReview.Application/Constants/EmailConstants.cs
+++ Core/GraphReview.Application/Constants/EmailConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GraphReview.Application.Constants
+﻿namespace GraphReview.Application.Constants
 {
     public static class EmailConstants
     {
